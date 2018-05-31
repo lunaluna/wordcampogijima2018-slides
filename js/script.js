be73ef69ebf16d933ee7bd2163565b687c14bd6f
lunaluna <web@lunalunadesign.com>
2018-05-31 09:40:47 +0900

    add in-page scroll / unfinished

---
+++ js/script.js
@@ -0,0 +1,34 @@
+'use strict';
+(function($){
+  //URLのハッシュ値を取得
+  var urlHash = location.hash;
+  //ハッシュ値があればページ内スクロール
+  if(urlHash){
+    //スクロールを0に戻す
+    $('body,html').stop().scrollLeft(0);
+    setTimeout(function(){
+      //ロード時の処理を待ち、時間差でスクロール実行
+      scrollToAnker(urlHash) ;
+    }, 100);
+  }
+
+  //通常のクリック時
+  $('a[href^="#"]').on('click', function(){
+    //ページ内リンク先を取得
+    var href= $(this).attr("href");
+    //リンク先が#か空だったらhtmlに
+    var hash = href == "#" || href == "" ? 'html' : href;
+    //スクロール実行
+    scrollToAnker(hash);
+    //リンク無効化
+    return false;
+  });
+
+  // 関数：スムーススクロール
+  // 指定したアンカー(#ID)へアニメーションでスクロール
+  function scrollToAnker(hash) {
+    var target = $(hash);
+    var position = target.offset().left;
+    $('body,html').stop().animate({scrollLeft:position}, 500);
+  }
+})(jQuery);
